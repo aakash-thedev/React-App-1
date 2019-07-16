e1857bde93cb76e054c3d6019b02eb2cb1d063d6
aakash-thedev <akghostrider008@gmail.com>
2019-07-16 15:54:02 +0530

    Created React App

---
+++ precache-manifest.1131b3070c7f4719d7f11a736abd90d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34cd1d1c9240d9a0f0ef6764a6feb2df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d880db657da275d0114c",
+    "url": "/static/css/main.1754a2be.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "d880db657da275d0114c",
+    "url": "/static/js/main.1f7da436.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
